821fad467516c1708e6f45d86c2d7194d90befa6
Elena <elena.gordiyenko@gmail.com>
2016-04-04 02:09:38 +0300

    ch04 E01

---
+++ src/ch04_object/E01_PrimitiveDefaultInitialization.java
@@ -0,0 +1,10 @@
+package ch04_object;
+
+public class E01_PrimitiveDefaultInitialization {
+    private static int k;
+    private static char z;
+
+    public static void main(String[] args) {
+        System.out.println("Class variables: " + k + " char:" + z + ".");
+    }
+}
