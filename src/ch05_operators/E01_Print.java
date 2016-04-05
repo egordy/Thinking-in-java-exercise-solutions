e6a9d859a55a291ae97515a65bb0e66495b6f1a8
Elena <elena.gordiyenko@gmail.com>
2016-04-05 22:15:03 +0300

    ch05_E01Print

---
+++ src/ch05_operators/E01_Print.java
@@ -0,0 +1,13 @@
+package ch05_operators;
+
+//import static net.mindview.util.Range.*;
+
+import static net.mindview.util.Print.print;
+
+public class E01_Print {
+    public static void main(String[] args) {
+        System.out.println("What");
+        print("ever");
+    }
+
+}
